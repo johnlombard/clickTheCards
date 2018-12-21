61931d0978da50bb0a9dad48e1d8d309720b089a
John Lombard <John@Johns-MacBook-Pro.local>
2018-12-20 21:07:24 -0500

    Updates

---
+++ precache-manifest.46bf883d6e21e3817cd682a82593f041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0d0f4b324c48c2506b9",
+    "url": "/johnlombard/clickTheCards/static/js/runtime~main.e0d0f4b3.js"
+  },
+  {
+    "revision": "25b2859d712a34a6f02d",
+    "url": "/johnlombard/clickTheCards/static/js/main.25b2859d.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/johnlombard/clickTheCards/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "25b2859d712a34a6f02d",
+    "url": "/johnlombard/clickTheCards/static/css/main.9c266878.chunk.css"
+  },
+  {
+    "revision": "f3323ad83bbc7fb1103c04cbfca521b9",
+    "url": "/johnlombard/clickTheCards/index.html"
+  }
+];
