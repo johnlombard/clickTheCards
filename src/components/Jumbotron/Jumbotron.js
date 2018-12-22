37a76d5c367daf1245ba7e82b5ce592e678f7880
John Lombard <John@Johns-MBP.fios-router.home>
2018-12-22 13:30:01 -0500

    jumbotron update

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Jumbotron = () => (
   <div className="jumbotron">
-    <h1>Your Project</h1>
+    <h1>Reading is Cool</h1>
     <p>
       Click to read one of my favorite books. If you read the same book twice, you'll have to restart! Try to read all 15.
     </p>
